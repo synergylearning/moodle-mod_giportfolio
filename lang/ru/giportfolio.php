fbdca3a38c55fa77f1333e2b090141ff69b24920
Pavel E. Timoshenko <P.E.Timoshenko@GMail.com>
2020-01-13 11:56:53 +0300

    Added support for the Russian language

---
+++ lang/ru/giportfolio.php
@@ -0,0 +1,186 @@
+<?php
+// This file is part of giportfolio plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * giportfolio module language strings
+ *
+ * @package    mod_giportfolio
+ * @copyright  2020 Pavel E. Timoshenko
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addafter'] = 'Добавить новый раздел';
+$string['addcontent'] = 'Пожалуйста введите инструкции для студентов';
+$string['addcontrib'] = 'Добавить достижение';
+$string['allusers'] = 'Все студенты';
+$string['attachment'] = 'Вложения';
+$string['automaticgrading'] = 'Автоматическое оценивание';
+$string['chapternumber'] = 'Количество разделов:  ';
+$string['chapternumberinit'] = 'Начальное количество разделов:  ';
+$string['chapters'] = 'Разделы';
+$string['chapterscount'] = 'Разделы';
+$string['chaptertitle'] = 'Название раздела';
+$string['collapsesubchapters'] = 'Свернуть подразделы';
+$string['confchapterdelete'] = 'Вы действительно хотите удалить этот раздел?';
+$string['confchapterdeleteall'] = 'Вы действительно хотите удалить этот раздел и все его подразделы?';
+$string['confcontribdelete'] = 'Вы уверены, что хотите удалить это достижение?';
+$string['content'] = 'Содержание';
+$string['continuecontrib'] = 'Продолжить добавлять достижения';
+$string['contribadded'] = 'Достижение добавлено';
+$string['contribaddform'] = 'Добавить новое достижение ';
+$string['contribcontent'] = 'Содержание';
+$string['contribmodform'] = 'Обновить достижение ';
+$string['contribupdated'] = 'Достижение обновлено';
+$string['contribution'] = 'достижение';
+$string['contributioncount'] = 'Показать количество достижений';
+$string['contributioncount_desc'] = 'Для пользователей, которые могут добавлять достижения, это будет отображаться на странице курса рядом с названием занятия';
+$string['contributions'] = 'Достижения';
+$string['contributiontitle'] = 'Название';
+$string['courseoverview'] = 'Отчет об активности моего курса';
+$string['customtitles'] = 'Пользовательские названия';
+$string['customtitles_help'] = 'Названия глав отображаются автоматически только в оглавлении.';
+$string['deletecontribwarning'] = 'Этот раздел содержит {$a} достижений. Если продолжите, то все они будут удалены без возможности восстановления.';
+$string['deletecontribwarningsub'] = 'Этот раздел вместе с подразделами содержит {$a} достижений. Если продолжите, то все они будут удалены без возможности восстановления.';
+$string['editingchapter'] = 'Правка раздела';
+$string['editinguserchapter'] = 'Править раздел';
+$string['edityourchapters'] = 'Редактировать ваши разделы';
+$string['emailmessagecourse'] = 'внутри курса:  ';
+$string['emailmessageuser'] = 'Портфолио был опубликован пользователем:  ';
+$string['emailsubject'] = 'Портфолио опубликован в вашем курсе';
+$string['errorchapter'] = 'Ошибка чтения раздела портфолио.';
+$string['eventchaptercreated'] = 'Раздел создан';
+$string['eventchapterdeleted'] = 'Раздел удален';
+$string['eventchapterupdated'] = 'Раздел обновлен';
+$string['eventchapterviewed'] = 'Раздел просмотрен';
+$string['exportpdf'] = 'Экспортировать в PDF';
+$string['exportzip'] = 'Экспортировать в ZIP';
+$string['faq'] = 'ЧаВо для портфолио';
+$string['faq_help'] = '
+**
+Пожалуйста, измените название раздела и содержание раздела по умолчанию.
+После того, как студент добавит материалы, вы не можете добавлять главы. Возможно будет только изменять текст шаблона существующих глав.
+
+Инструкции по использованию портфолио...
+';
+$string['feedbackupdated'] = '...Отзыв обновлен...';
+$string['filterlist'] = '<h3>Фильтровать список пользователей</h3>';
+$string['giportfolio:addinstance'] = 'Добавить новое портфолио';
+$string['giportfolio:edit'] = 'Редактировать разделы портфолио';
+$string['giportfolio:editgiportfolios'] = 'Добавить/Редактировать структуру';
+$string['giportfolio:gradegiportfolios'] = 'Оценить портфолио';
+$string['giportfolio:printclassplan'] = 'Распечатать план';
+$string['giportfolio:submitportfolio'] = 'Отправить портфолио';
+$string['giportfolio:view'] = 'Просмотр портфолио';
+$string['giportfolio:viewgiportfolios'] = 'Просмотр других портфолио';
+$string['giportfolio:viewhiddenchapters'] = 'Показать скрытые разделы портфолио';
+$string['giportfolioof'] = 'Портфолио: ';
+$string['gradesupdated'] = '...Оценки обновлены...';
+$string['gradeupdated'] = 'Оценки обновлены ';
+$string['hide'] = 'Скрыть ваше достижение от остальных, включая проверяющих';
+$string['hideshared'] = 'Скрыть достижения других студентов';
+$string['insertgrade'] = 'Оценить портфолио';
+$string['klassenbuchtrainer'] = 'Режим &laquo;план класса&raquo;';
+$string['klassenbuchtrainer_help'] = 'Включение этого параметра отобразит &laquo;план класса&raquo; в каждом разделе портфолио. <br />
+Эту опцию можно выбрать только при создании нового портфолио.';
+$string['lastgrade'] = 'Оценено ';
+$string['lastmodified'] = 'Последнее изменение:  ';
+$string['lastupdated'] = 'Последнее обновление:  ';
+$string['messageprovider:addentry'] = 'Уведомление о новых записях портфолио';
+$string['modulename'] = 'Портфолио';
+$string['modulename_help'] = '<p>Модуль портфолио позволяет студентам вносить достижения в портфолио, структура которого устанавливается преподавателем.</p>
+<p>Преподаватели курса могут создать общую структуру для работы учащихся с помощью разделов и подразделов, аналогично макету, определенному в модуле книги.</p>
+<p>После студенты курса могут добавлять свои собственные достижения на основе этой структуры, которые затем могут комментировать и оценивать преподаватели курса.</p>
+';
+$string['modulenameplural'] = 'Портфолио';
+$string['myactivitylink'] = 'Показать \'Отчет о моей активности\'';
+$string['name'] = 'Название: ';
+$string['navexit'] = 'Выйти из портфолио';
+$string['navnext'] = 'Следующий';
+$string['navprev'] = 'Предыдущий';
+$string['nocomments'] = 'Без комментариев преподавателя';
+$string['nosubmisson'] = 'Записи отсутствуют';
+$string['notifyaddentry'] = 'Уведомление преподавателей о новых записях';
+$string['notifyaddentry_subject'] = 'Новое достижение в портфолио от {$a}';
+$string['notifyaddentry_body'] = 'Здравствуйте!
+
+Студент {$a->username} добавил новое достижение в раздел \'{$a->chapter}\' портфолио \'{$a->portfolio}\' в курсе \'{$a->course}\'.
+
+Вы можете просмотреть его, перейдя по ссылке: {$a->link}';
+$string['notstarted'] = 'Не начинал';
+$string['notyourchapter'] = 'Вы не являетесь владельцем этого раздела';
+$string['numbering'] = 'Нумерация разделов';
+$string['numbering_help'] = '* None - chapter and subchapter titles are not formatted at all, use if you want to define special numbering styles. For example letters: in chapter title type "A First Chapter", "A.1 Some Subchapter",...
+* Numbers - разделы и подразделы нумеруются (1, 1.1, 1.2, 2, ...)
+* Маркеры - подразделы содержат отступ и маркеры
+* Отступы - разделы с отступом';
+$string['numbering0'] = 'Отсутствует';
+$string['numbering1'] = 'Нумерация';
+$string['numbering2'] = 'Маркеры';
+$string['numbering3'] = 'Отступы';
+$string['numberingoptions'] = 'Доступные варианты нумерации';
+$string['numberingoptions_help'] = 'Выберите параметры нумерации, которые должны быть доступны при создании новых портфелей.';
+$string['optionalsettings'] = 'Дополнительные настройки';
+$string['page-mod-giportfolio-x'] = 'Любая страница модуля портфолио';
+$string['pagesize'] = 'Количество элементов на страницу';
+$string['participantadd'] = 'Участники могут добавлять разделы';
+$string['peersharing'] = 'Участники могут иметь совместные достижения';
+$string['peersharing_help'] = 'Если опция включена, то Участники могут разделять свои достижения с другими';
+$string['pluginadministration'] = 'Администрирование портфолио';
+$string['pluginname'] = 'Портфолио';
+$string['printing'] = 'Отключить печать';
+$string['printing_help'] = 'Отключить печать';
+$string['private'] = 'Конфиденциальный';
+$string['publish'] = 'Опубликовать';
+$string['publishnotification'] = 'Отправить уведомление о публикации';
+$string['publishupdated'] = 'Статус публикации обновлен';
+$string['quickgrade'] = 'Разрешить быстрое оценивание';
+$string['quickgrade_help'] = 'Если опция включена, несколько заданий могут быть оценены на одной странице. Добавьте оценки и комментарии, затем нажмите кнопку «Сохранить все мои отзывы», чтобы сохранить все изменения для этой страницы.';
+$string['saveallfeedback'] = 'Сохранить все';
+$string['share'] = 'Поделиться с другими студентами';
+$string['show'] = 'Поделиться своим достижением, но не со студентами';
+$string['showshared'] = 'Просмотр достижений других студентов';
+$string['showtimeofday'] = 'Показывать время суток по датам';
+$string['noshared'] = 'Отсутствуют разделяемые достижения других студентов';
+$string['sincelastlogin'] = 'Обновлено с момента вашего последнего входа';
+$string['skipintro'] = 'Студенты пропускают вступительную страницу';
+$string['startcontrib'] = 'Начать вносить достижения  ';
+$string['stopedit'] = 'Выключить редактирование';
+$string['studentgiportfolio'] = 'Портфолио студента';
+$string['studentgiportfolios'] = '<h2>Портфолио студента</h2>';
+$string['subchapter'] = 'Подраздел';
+$string['submitedporto'] = 'Отправлено портфолио:  ';
+$string['toc'] = 'Оглавление';
+$string['top'] = 'наверх';
+$string['unpublish'] = 'Отменить публикацию';
+$string['unshare'] = 'Прекратить разделение достижения с другими студентами';
+$string['updatecontrib'] = 'Обновить достижение';
+$string['updatefailed'] = 'Ошибка обновления';
+$string['updating'] = 'Обновление...';
+$string['upgrade'] = '(Обновление)';
+$string['usergraded'] = 'Вы были оценены за эту деятельность:  ';
+$string['usergradefeedback'] = 'Отзыв получен:  ';
+$string['usertoc'] = 'Содержание';
+$string['view'] = 'Просмотр';
+$string['viewgiportfolio'] = 'Просмотр портфолио:  ';
+$string['viewtemplate'] = 'Просмотр/Правка шаблона портфолио';
+$string['visibility'] = 'Видимость';
+$string['visibilityexplain'] = 'установка видимости достижений';
+$string['visibilityexplain_help'] = '
+* Показать — достижение видно для пользователей, не являющихся студентами
+* Скрыть — достижение видно только самому пользователю
+';
